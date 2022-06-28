f5fc6d1397712cb737cdcce682428406624ae130
hagi torres <hagiraitorresmacedo@gmail.com>
2022-06-27 19:23:15 -0500

    feat(src/cardUser): create component card user

---
+++ src/components/cardUser.js
@@ -0,0 +1,71 @@
+class User extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+    }
+    static get observedAttributes() {
+      return ["name", "lastname", "email", "image"];
+    }
+    attributeChangedCallback(attr, oldVal, newVal) {
+      if (attr === "name") {
+        this.name = newVal;
+      }
+      if (attr === "lastname") {
+        this.lastname = newVal;
+      }
+      if (attr === "email") {
+        this.email = newVal;
+      }
+      if (attr === "image") {
+        this.image = newVal;
+      }
+    }
+    getTemplate() {
+      const template = document.createElement("template");
+      template.innerHTML = `
+            <section class="user-card">
+              <div>
+                  <h2>
+                  ${this.name}
+                  </h2>
+                  <h3>
+                  ${this.lastname}
+                  </h3>
+                  <h4>
+                  ${this.email}
+                  </h4>
+              </div>
+              <img src=${this.image}/>
+            </section>
+            ${this.getStyles()}
+          `;
+      return template;
+    }
+    getStyles() {
+      return `
+          <style>
+          .user-card{
+              background: blue
+          }
+              .user-card{
+                  margin: 15px;
+              }
+              h2 {
+              color: red;
+              }
+              img{
+                  width:100px;
+              }
+          </style>
+          `;
+    }
+    render() {
+      this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+    connectedCallback() {
+      this.render();
+    }
+  }
+  
+  customElements.define("user-card", User);
+  
